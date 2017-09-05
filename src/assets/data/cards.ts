55880465f7c663c6441476a81c3464892c1287d2
Katharine <krsibbald@gmail.com>
2017-09-04 21:31:42 -0400

    list of cards

--- src/assets/data/cards.ts
+++ src/assets/data/cards.ts
@@ -8,7 +8,8 @@ export const ALLCARDS: CardModel[] =
     reagent1: '1o Alkyl halide',
     reagent2: 'Electrophile',
     notes: 'Electrophile for SN2 reactions.',
-    image: 'assets/img/cards/large/card1.png'
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 3
   },{
     id: 2,
     name: '2-Chloro-2-methylpropane',
@@ -16,13 +17,214 @@ export const ALLCARDS: CardModel[] =
     reagent1: '3o Alkyl halide',
     reagent2: 'Electrophile',
     notes: 'Electrophile for SN1, E1, and E2 reactions.',
-    image: 'assets/img/cards/large/card1.png'
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 4
   },{
     id: 3,
     name: 'Hydroxide',
     points: 1,
     reagent1: 'Strong Base',
     reagent2: 'Strong Nucleophile',
-    notes: ' Nucleophile/base for SN2 and E2 reactions.',
-    image: 'assets/img/cards/large/card1.png'
+    notes: 'Nucleophile/base for SN2 and E2 reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 4
+  },{
+    id: 4,
+    name: 'Chlorine',
+    points: 2,
+    reagent1: 'Oxidant',
+    reagent2: '',
+    notes: 'Strong oxidant capable of reacting with alkenes and under radical conditions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 5,
+    name: 'Hydrochloric Acid',
+    points: 1,
+    reagent1: 'Strong Acid',
+    reagent2: '',
+    notes: 'Strong acid useful for protonation of many weakly basic molecules. Chloride ion produced after deprotonation.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 6,
+    name: '1-Butene',
+    points: 1,
+    reagent1: 'Alkene',
+    reagent2: '',
+    notes: 'Alkene useful for addition reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 7,
+    name: 'Light',
+    points: 3,
+    reagent1: 'Catalyst',
+    reagent2: '',
+    notes: 'Catalyst for initiating photochemical reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 8,
+    name: 'Magnesium metal',
+    points: 5,
+    reagent1: 'Metal',
+    reagent2: '',
+    notes: 'Reacts with alkyl halides to form Grignard reagents.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 9,
+    name: 'Thionyl chloride',
+    points: 3,
+    reagent1: 'Chlorinating Reagent',
+    reagent2: '',
+    notes: 'Converts alcohols into alkyl chlorides.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 10,
+    name: 'Sodium Hypochlorite',
+    points: 3,
+    reagent1: 'Oxidant',
+    reagent2: '',
+    notes: 'Converts alcohols into carbonyls.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 11,
+    name: 'Swern Conditions',
+    points: 3,
+    reagent1: 'Oxidant',
+    reagent2: '',
+    notes: 'Converts alcohols into carbonyls.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 12,
+    name: '4-Penten-1-ol',
+    points: 5,
+    reagent1: 'Alcohols',
+    reagent2: 'Alkene',
+    notes: 'Contains both a primary alcohol and alkene.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 13,
+    name: 'Sodium Hydride',
+    points: 2,
+    reagent1: 'Strong Base',
+    reagent2: '',
+    notes: 'A strong base without a significant reverse reaction.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 14,
+    name: 'Lithium Aluminum Hydride',
+    points: 3,
+    reagent1: 'Reducing Agent',
+    reagent2: '',
+    notes: 'A strong reducing agent commonly used with carbonyls.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 15,
+    name: 'Lithium diisopropyl amine.',
+    points: 3,
+    reagent1: 'Strong Base',
+    reagent2: '',
+    notes: 'A strong base useful in E2 and deprotonation reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 16,
+    name: 'Sodium cyanide',
+    points: 2,
+    reagent1: 'Weak Base',
+    reagent2: 'Strong Nucleophile',
+    notes: 'An unhindered nucleophile especially suited for SN2  reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 17,
+    name: 'Hydronium Ion',
+    points: 1,
+    reagent1: 'Weak Acid',
+    reagent2: '',
+    notes: 'Weak acid useful for protonation of basic molecules.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+   },{
+    id: 18,
+    name: 'n-Propylamine',
+    points: 1,
+    reagent1: 'Weak Base',
+    reagent2: 'Weak Nucleophile',
+    notes: 'Nucleophile/base for SN1 and E1 reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+   },{
+    id: 19,
+    name: 'Ammonia',
+    points: 1,
+    reagent1: 'Weak Base',
+    reagent2: 'Weak Nucleophile',
+    notes: 'Nucleophile/base for SN1 and E1 reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 20,
+    name: 'Ozone',
+    points: 1,
+    reagent1: 'Oxidant',
+    reagent2: '',
+    notes: 'A powerful oxidant capable of cleaving alkenes and alkynes.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 21,
+    name: '1,3-Butadiene',
+    points: 4,
+    reagent1: 'Alkene',
+    reagent2: '',
+    notes: 'A conjugated diene.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 22,
+    name: '2-Chloropropane',
+    points: 2,
+    reagent1: '2o Alkyl halide',
+    reagent2: 'Electrophile',
+    notes: 'Electrophile for SN1, SN2, E1, and E2 reactions.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 23,
+    name: 'Acrolein',
+    points: 5,
+    reagent1: 'Carbonyl',
+    reagent2: 'Alkene',
+    notes: 'A good Michael acceptor and Diels-Alder reagent.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 24,
+    name: 'Cyclopentadiene',
+    points: 4,
+    reagent1: 'Alkene',
+    reagent2: '',
+    notes: 'A cyclic conjugated diene.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+  },{
+    id: 25,
+    name: 'Dimethyl maleate',
+    points: 1,
+    reagent1: 'Alkene',
+    reagent2: 'Carbonyl',
+    notes: 'A electron-deficient alkene.',
+    image: 'assets/img/cards/large/card1.png',
+    startHandNum: 0
+
   }];
